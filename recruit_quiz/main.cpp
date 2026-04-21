fdf8c291343f6b81ef937914c2e4e265efe3fd40
youo516 <vantan.sakane16@gmail.com>
2026-04-21 12:17:47 +0900

    ランダムに数式が変化

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <random>
 using namespace std;
 
 //
@@ -11,11 +12,33 @@ struct Question
 
 int main()
 {
-	Question questions[] = {
-		{"13 x (-5)", 13 * -5},
-		{"(-21) ÷ (-3)", -21 / -3},
-		{"7 - (4 + 2) ÷ 2", 7 - (4 + 2) / 2},
-	};
+	Question questions[3];
+
+	random_device rd;
+	mt19937 rand(rd());
+
+	int x;
+	int y;
+	int z;
+	int w;
+
+	//乗算
+	x = uniform_int_distribution<>(1, 30)(rand);
+	y = uniform_int_distribution<>(1, 20)(rand);
+	questions[0].q = to_string(x) + "x" + to_string(y);
+	questions[0].a = x * y;
+
+	//除算
+	x = uniform_int_distribution<>(1, 30)(rand);
+	y = uniform_int_distribution<>(1, 20)(rand);
+	questions[1].q = to_string(x * y) + "÷" + to_string(y);
+	questions[1].a = x / y;
+
+	//複雑な式
+	x = uniform_int_distribution<>(1, 100)(rand);
+	y = z = w = uniform_int_distribution<>(1, 10)(rand);
+	questions[2].q = to_string(x) + "-(" + to_string(y * w) + "+" + to_string(z * w) + ")÷" + to_string(w);
+	questions[2].a = x - (y + z);
 
 	cout << "[リクルート試験対策クイズ]\n";
 
