44d86fdba2021dad88134b2b1a68572785adc6a1
youo516 <vantan.sakane16@gmail.com>
2026-04-28 12:42:20 +0900

    組み合わせの問題を追加

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -86,6 +86,20 @@ int main()
 	}
 	questions.push_back({ to_string(x) + "人のうち" + to_string(y) + "人を選んで並べる方法は何通りあるか調べよ。", to_string(z) });
 
+	//組み合わせ
+	x = uniform_int_distribution<>(3, 6)(rand);
+	y = uniform_int_distribution<>(2, x)(rand);
+	z = 1;
+	for (int i = 0; i < y; i++)
+	{
+		z *= x - i;
+	}
+	for (int i = 0; i < y; i++)
+	{
+		z /= y - 1;
+	}
+	questions.push_back({ to_string(x) + "人のうち" + to_string(y) + "人を選ぶ組み合わせは何通りあるか調べよ。", to_string(z) });
+
 	cout << "[リクルート試験対策クイズ]\n";
 
 	for (const auto& e : questions)
