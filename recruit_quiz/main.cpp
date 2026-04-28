6f1bca5e3a7dcd771a48ce35bac595ef6caade29
youo516 <vantan.sakane16@gmail.com>
2026-04-28 11:59:50 +0900

    サイコロの確率を求める問題を追加

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -11,6 +11,19 @@ struct Question
 	string a;
 };
 
+//最大公約数を求める
+int gcd(int a, int b)
+{
+	//余りが0になった時の除数を返す
+	while (b)
+	{
+		int r = a % b;
+		a = b;
+		b = r;
+	}
+	return a;
+}
+
 int main()
 {
 	vector<Question> questions(3);
@@ -56,6 +69,12 @@ int main()
 	//球の体積
 	x = uniform_int_distribution<>(1, 5)(rand) * 3;
 	questions.push_back({ "半径" + to_string(x) + "cmの球がある。\n" + "この球の体積をXπcm^3としたとき、Xの値を求めよ。", to_string(x * x * x * 4 / 3) });
+
+	//サイコロの確率
+	x = uniform_int_distribution<>(1, 5)(rand);
+	y = uniform_int_distribution<>(1, 6 - x)(rand);
+	z = gcd(y + 1, 6);
+	questions.push_back({ "サイコロを1個振って、" + to_string(x) + "から" + to_string(x + y) + "が出る確率を求めよ。", to_string((y + 1) / z) + "/" + to_string(6 / z) });
 
 	cout << "[リクルート試験対策クイズ]\n";
 
