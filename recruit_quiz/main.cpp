e32b9914c646eded45745340d38eab0190e970dd
youo516 <vantan.sakane16@gmail.com>
2026-04-28 11:07:22 +0900

    円錐の体積を求める問題を追加

--- recruit_quiz/main.cpp
+++ recruit_quiz/main.cpp
@@ -48,6 +48,11 @@ int main()
 	y = uniform_int_distribution<>(1, 5)(rand) * 2;
 	questions.push_back({ "面積" + to_string(x * y / 2) + "cm^2、底辺" + to_string(y) + "cmの三角形の高さを求めよ。", x });
 
+	//円錐の体積
+	x = uniform_int_distribution<>(1, 10)(rand);
+	y = uniform_int_distribution<>(1, 5)(rand) * 2;
+	questions.push_back({ "底面の半径" + to_string(x) + "cm、高さ" + to_string(y) + "cmの円錐がある。\n" + "この円錐の体積をXπcm^3としたとき、Xの値を求めよ。", x * x * y / 3 });
+
 	cout << "[リクルート試験対策クイズ]\n";
 
 	for (const auto& e : questions)
