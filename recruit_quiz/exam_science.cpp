0aca69aa6437c3abd5d8457ff4fef10f70bbab45
youo516 <vantan.sakane16@gmail.com>
2026-06-30 11:35:04 +0900

    等加速度直線運動の問題を追加

--- recruit_quiz/exam_science.cpp
+++ recruit_quiz/exam_science.cpp
@@ -10,6 +10,7 @@ QuestionList CreatePhysicsExam()
 	questions.reserve(10);
 	random_device rd;
 
+	//等速直線運動
 	{
 		constexpr int divisors[] = { 1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30, 60 };
 		int i = uniform_int_distribution<>(0, size(divisors) - 1)(rd);
@@ -21,6 +22,27 @@ QuestionList CreatePhysicsExam()
 		v = uniform_int_distribution<>(1, 10)(rd) * 60 / divisors[i];	//時速
 		t = uniform_int_distribution<>(1, 10)(rd) * divisors[i];		//時間
 		questions.push_back({ "ある車が" + to_string(v * t / 60) + "km離れた地点まで移動するのに" + to_string(t) + "分かかった。\nこの車の平均時速を求めよ", to_string(v) });
+	}
+
+	//等加速度直線運動
+	{
+		//v = v0 + at
+		int v0 = uniform_int_distribution<>(0, 10)(rd);	//初速
+		int a = uniform_int_distribution<>(1, 5)(rd);	//加速度
+		int t = uniform_int_distribution<>(1, 20)(rd);	//移動時間
+		questions.push_back({ "秒速" + to_string(v0) + "mで移動していた車が、進行方向に大きさ" + to_string(a) + "m/sで等加速度直線運動を始めた。\n" + to_string(t) + "秒後の速度m/sを求めよ。", to_string(v0 + a * t)});
+
+		//x = v0t + 1/2 * at^2
+		v0 = uniform_int_distribution<>(5, 20)(rd);
+		a = uniform_int_distribution<>(1, 5)(rd);
+		t = uniform_int_distribution<>(1, 10)(rd);
+		questions.push_back({ "秒速" + to_string(v0) + "mで移動していた車が、進行方向に大きさ" + to_string(a) + "m/sで等加速度直線運動を始めた。\n加速を始めてから" + to_string(t) + "秒後までに移動した距離をm単位で求めよ。", to_string(v0 * t + a * t * t / 2) });
+
+		//v^2 - v0^2 = 2ax
+		a = -uniform_int_distribution<>(1, 5)(rd) * 2;
+		t = uniform_int_distribution<>(1, 10)(rd);
+		v0 = -a * t;
+		questions.push_back({ "秒速" + to_string(v0) + "mで移動していた車がブレーキをかけたところ、"+ to_string(v0 * t + a * t * t / 2) + "m進んで停止した。\nブレーキの加速度m/s^2を求めよ。", to_string(a) });
 	}
 	return questions;
 }
