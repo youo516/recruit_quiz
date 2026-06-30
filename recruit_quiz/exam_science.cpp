084e16158f531d04106f32d92f17286ebb5a3209
youo516 <vantan.sakane16@gmail.com>
2026-06-30 11:00:36 +0900

    時速を求める問題を追加

--- recruit_quiz/exam_science.cpp
+++ recruit_quiz/exam_science.cpp
@@ -16,6 +16,11 @@ QuestionList CreatePhysicsExam()
 		int v = uniform_int_distribution<>(1, 10)(rd) * 60 / divisors[i];	//時速
 		int t = uniform_int_distribution<>(1, 10)(rd) * divisors[i];		//時間
 		questions.push_back({ "時速" + to_string(v) + "kmで移動する車がある。\nこの車が" + to_string(v * t / 60) + "km移動するために必要な時間を分単位で求めよ", to_string(t) });
+
+		i = uniform_int_distribution<>(0, size(divisors) - 1)(rd);
+		v = uniform_int_distribution<>(1, 10)(rd) * 60 / divisors[i];	//時速
+		t = uniform_int_distribution<>(1, 10)(rd) * divisors[i];		//時間
+		questions.push_back({ "ある車が" + to_string(v * t / 60) + "km離れた地点まで移動するのに" + to_string(t) + "分かかった。\nこの車の平均時速を求めよ", to_string(v) });
 	}
 	return questions;
 }
