aaf051c8c5038e29e3eb8ced912b7d360bf18078
youo516 <vantan.sakane16@gmail.com>
2026-08-25 10:43:49 +0900

    数学の問題をexam_mathematicsファイルに移動

---
+++ recruit_quiz/exam_mathematics.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "question.h"
+
+//”Šw‚Ì–â‘è‚ğì¬
+QuestionList CreateMathematicsExam();
---
+++ recruit_quiz/exam_mathematis.cpp
@@ -0,0 +1,97 @@
+#include "exam_mathematics.h"
+#include "utility.h"
+#include <random>
+using namespace std;
+
+//最大公約数を求める
+int gcd(int a, int b)
+{
+	//余りが0になった時の除数を返す
+	while (b)
+	{
+		int r = a % b;
+		a = b;
+		b = r;
+	}
+	return a;
+}
+
+//数学の問題を作成
+QuestionList CreateMathematicsExam()
+{
+	QuestionList questions(3);
+
+	random_device rd;
+	mt19937 rand(rd());
+
+	int x;
+	int y;
+	int z;
+	int w;
+
+	//乗算
+	x = uniform_int_distribution<>(1, 30)(rand);
+	y = uniform_int_distribution<>(1, 20)(rand);
+	questions[0].q = to_string(x) + "x" + to_string(y) + "の答えは？";
+	questions[0].a = to_string(x * y);
+
+	//除算
+	x = uniform_int_distribution<>(1, 30)(rand);
+	y = uniform_int_distribution<>(1, 20)(rand);
+	questions[1].q = to_string(x * y) + "÷" + to_string(y) + "の答えは？";
+	questions[1].a = to_string(x);
+
+	//複雑な式
+	x = uniform_int_distribution<>(1, 100)(rand);
+	y = uniform_int_distribution<>(1, 10)(rand);
+	z = uniform_int_distribution<>(1, 10)(rand);
+	w = uniform_int_distribution<>(1, 10)(rand);
+	questions[2].q = to_string(x) + "-(" + to_string(y * w) + "+" + to_string(z * w) + ")÷" + to_string(w) + "の答えは？";
+	questions[2].a = to_string(x - (y + z));
+
+	//三角形の面積
+	x = uniform_int_distribution<>(1, 10)(rand);
+	y = uniform_int_distribution<>(1, 5)(rand) * 2;
+	questions.push_back({ "面積" + to_string(x * y / 2) + "cm^2、底辺" + to_string(y) + "cmの三角形の高さを求めよ。", to_string(x) });
+
+	//円錐の体積
+	x = uniform_int_distribution<>(1, 5)(rand);
+	y = uniform_int_distribution<>(1, 5)(rand) * 3;
+	questions.push_back({ "底面の半径" + to_string(x) + "cm、高さ" + to_string(y) + "cmの円錐がある。\n" + "この円錐の体積をXπcm^3としたとき、Xの値を求めよ。", to_string(x * x * y / 3) });
+
+	//球の体積
+	x = uniform_int_distribution<>(1, 5)(rand) * 3;
+	questions.push_back({ "半径" + to_string(x) + "cmの球がある。\n" + "この球の体積をXπcm^3としたとき、Xの値を求めよ。", to_string(x * x * x * 4 / 3) });
+
+	//サイコロの確率
+	x = uniform_int_distribution<>(1, 5)(rand);
+	y = uniform_int_distribution<>(1, 6 - x)(rand);
+	z = gcd(y + 1, 6);
+	questions.push_back({ "サイコロを1個振って、" + to_string(x) + "から" + to_string(x + y) + "が出る確率を求めよ。", to_string((y + 1) / z) + "/" + to_string(6 / z) });
+
+	//順列
+	x = uniform_int_distribution<>(3, 7)(rand);
+	y = uniform_int_distribution<>(2, x)(rand);
+	z = 1;
+	for (int i = 0; i < y; i++)
+	{
+		z *= x - i;
+	}
+	questions.push_back({ to_string(x) + "人のうち" + to_string(y) + "人を選んで並べる方法は何通りあるか調べよ。", to_string(z) });
+
+	//組み合わせ
+	x = uniform_int_distribution<>(3, 6)(rand);
+	y = uniform_int_distribution<>(2, x)(rand);
+	z = 1;
+	for (int i = 0; i < y; i++)
+	{
+		z *= x - i;
+	}
+	for (int i = 0; i < y; i++)
+	{
+		z /= y - 1;
+	}
+	questions.push_back({ to_string(x) + "人のうち" + to_string(y) + "人を選ぶ組み合わせは何通りあるか調べよ。", to_string(z) });
+
+	return questions;
+}
